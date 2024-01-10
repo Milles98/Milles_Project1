1059dd8c2c54fd9c0894c89e13e92be481f89063
Mille Elfver <mille.elfver98@gmail.com>
2024-01-10 12:54:27 +0100

    changed max calc numbers clearer with commas

--- Milles_Project1Library/Migrations/20240110114332_test.Designer.cs
+++ Milles_Project1Library/Migrations/20240110114332_test.Designer.cs
@@ -12,7 +12,7 @@ using Milles_Project1Library.Data;
 namespace Milles_Project1Library.Migrations
 {
     [DbContext(typeof(ProjectDbContext))]
-    [Migration("20240110093407_test")]
+    [Migration("20240110114332_test")]
     partial class test
     {
         /// <inheritdoc />
